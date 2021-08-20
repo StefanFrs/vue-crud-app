42a8d4eaf1d00b7c8362f5d770514c3a96fdf5fe
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2021-08-20 13:51:43 +0300

    Added style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <nav class="navbar navbar-dark bg-dark justify-content-between flex-nowrap flex-row">
+    <nav
+      class="navbar navbar-dark bg-dark justify-content-between flex-nowrap flex-row"
+    >
       <div class="container">
-        <a class="navbar-brand float-left">Firebase Vue CRUD Example</a>
+        <a class="navbar-brand float-left">Employee registration</a>
         <ul class="nav navbar-nav flex-row float-right">
           <li class="nav-item">
             <router-link class="nav-link pr-3" to="/">Add User</router-link>
@@ -13,9 +15,24 @@
         </ul>
       </div>
     </nav>
-
-    <div class="container mt-5">
-      <router-view></router-view>
+    <div class="background">
+      <div class="container pt-5">
+        <router-view></router-view>
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+.background {
+  background: linear-gradient(
+    115deg,
+    rgba(0, 25, 82, 0.8),
+    rgba(32, 4, 64, 0.7)
+  );
+  height: 100vh;
+}
+label, p,h1,h2,h3,h4,h5{
+  color: white;
+}
+</style>
